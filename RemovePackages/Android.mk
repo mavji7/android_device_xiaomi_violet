1cf4beb91b53e65a49cb46cc3f2e560f77df354d
mavji7 <mehtavishwas7@gmail.com>
2021-06-17 13:55:00 +0000

    violet: Removed useless gapps packages
    Signed-off-by: mavji7 <mehtavishwas7@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,8 +4,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube GoogleTTS SoundAmplifierPrebuilt Photos GoogleCamera arcore talkback FilesPrebuilt RecorderPrebuilt FM2 
-LOCAL_UNINSTALLABLE_MODULE := true 
+LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube GoogleTTS SoundAmplifierPrebuilt GoogleCamera arcore talkback FilesPrebuilt RecorderPrebuilt FM2 PixelWallpapers2020 Tycho Ornament SafetyHubPrebuilt WallpapersBReel2020 PixelLiveWallpaperPrebuilt MicropaperPrebuilt arcore AmbientSensePrebuilt AppDirectedSMSService CarrierSetup ConnMO DCMO DMService MyVerizonServices OBDM_Permissions obdm_stub Showcase SprintDM SprintHM USCCDM VZWAPNLib VzwOmaTrigger 
+LOCAL_UNINSTALLABLE_MODULE := true
+>>>>>>> 94e63e50... violet: Removed more & more useless gapps packages
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
 include $(BUILD_PREBUILT)
